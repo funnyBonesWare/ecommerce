3adfc7f3366661b2a7b6aa53e91178e916ea8436
funnybonesware <sahilsiddiquihere@gmail.com>
2025-08-30 17:37:06 +0530

    Refactor Navbar component from functional to class-based component for improved state management and lifecycle methods.

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './styles.css';
 
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        E-commerce
-      </div>
-    </nav>
-  );
-};
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="navbar-container">
+          E-commerce
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
